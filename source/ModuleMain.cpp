e04a73a0470b83153d6f82c52fc1e94a2c0a47d3
XxDavid123xX <137454382+XxDavid123xX@users.noreply.github.com>
2026-01-03 16:16:34 -0400

    Last Sync: 2026-01-03 16:16 (Mobile)

---
+++ source/ModuleMain.cpp
@@ -0,0 +1,51 @@
+#include <YYToolkit/YYTK_Shared.hpp>
+using namespace Aurie;
+using namespace YYTK;
+
+static YYTKInterface* g_ModuleInterface = nullptr;
+
+void FrameCallback(FWFrame& FrameContext)
+{
+	UNREFERENCED_PARAMETER(FrameContext);
+
+	static uint32_t frame_counter = 0;
+
+	if (frame_counter % 30 == 0)
+		g_ModuleInterface->PrintWarning("[Example Plugin] - 30 frames have passed! Framecount: %u", frame_counter);
+
+	frame_counter++;
+}
+
+EXPORTED AurieStatus ModuleInitialize(
+	IN AurieModule* Module,
+	IN const fs::path& ModulePath
+)
+{
+	UNREFERENCED_PARAMETER(ModulePath);
+
+	AurieStatus last_status = AURIE_SUCCESS;
+
+	// Gets a handle to the interface exposed by YYTK
+	// You can keep this pointer for future use, as it will not change unless YYTK is unloaded.
+	g_ModuleInterface = YYTK::GetInterface();
+
+	// If we can't get the interface, we fail loading.
+	if (!g_ModuleInterface)
+		return AURIE_MODULE_DEPENDENCY_NOT_RESOLVED;
+
+	g_ModuleInterface->Print(CM_LIGHTGREEN, "[Example Plugin] - Hello from PluginEntry!");
+
+	last_status = g_ModuleInterface->CreateCallback(
+		Module,
+		EVENT_FRAME,
+		FrameCallback,
+		0
+	);
+
+	if (!AurieSuccess(last_status))
+	{
+		g_ModuleInterface->Print(CM_LIGHTGREEN, "[Example Plugin] - Failed to register callback!");
+	}
+
+	return AURIE_SUCCESS;
+}
